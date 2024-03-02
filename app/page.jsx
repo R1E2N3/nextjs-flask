11f82692011f63b25c35b6e93f03f5963408e68d
R1E2N3 <78232754+R1E2N3@users.noreply.github.com>
2024-03-01 21:38:41 -0300

    f

--- app/page.jsx
+++ app/page.jsx
@@ -14,8 +14,6 @@ const Home = () => (
     <Link href='/test' className="ui_btn">
       Find the best options for you or your child.
     </Link>
-
-    <Link href='/testing' className='ui_btn block'>Click me</Link>
   </section>
 );
 
